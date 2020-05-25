7c2d52cdc7e0a80a01f2d323d6c8c5147464b6fb
Jamie Cargill <jamie@jamiecargill.com>
2020-05-25 23:03:08 +0100

    Getting activities from user and writing to a file

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import dotenv
+import json
+from stravaio import strava_oauth2, StravaIO
+
+dotenv.load_dotenv()
+
+STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
+
+token = strava_oauth2(client_id=STRAVA_CLIENT_ID, client_secret=STRAVA_CLIENT_SECRET)
+
+client = StravaIO(access_token=token['access_token'])
+
+athlete = client.get_logged_in_athlete()
+
+activities = client.get_logged_in_athlete_activities(after='last week')
+
+with open('activities.txt', 'w') as f:
+    for activity in activities:
+        f.write("%s\n" % activity)
