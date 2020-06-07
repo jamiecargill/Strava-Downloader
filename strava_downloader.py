13ce5c2f3429e537cb40aabde992a1d3145b01cf
Jamie Cargill <jamie@jamiecargill.com>
2020-06-07 13:40:04 +0100

    Splitting up a few functions

--- strava_downloader.py
+++ strava_downloader.py
@@ -5,53 +5,54 @@ import json
 import time
 from stravaio import strava_oauth2, StravaIO
 
-def get_all_activities(client):
-    activities = client.get_logged_in_athlete_activities()
-
-    activity_dict = {}
-
-    for activity in activities:
-        activity = activity.to_dict()
-        activity_dict[activity["id"]] = activity
-    
-    activity_json = json.dumps(activity_dict, indent=4, sort_keys=True, default=str)
-
-    return activity_json
-
-def get_activities_since_date(client, date):
-    activities = client.get_logged_in_athlete_activities(after=date)
-
-    activity_dict = {}
-
-    for activity in activities:
-        activity = activity.to_dict()
-        activity_dict[activity["id"]] = activity
-    
-    activity_json = json.dumps(activity_dict, indent=4, sort_keys=True, default=str)
-
-    return activity_json
-
+def write_activities_to_file(activity_json):
     file_activities = open("activities.json", "w")
     file_activities.write(activity_json)
     file_activities.close()
 
+def get_activities(client, date = None):
+    if date is None:
+        activities = client.get_logged_in_athlete_activities()
+    else:
+        activities = client.get_logged_in_athlete_activities(after=date)
+
+    activity_dict = {}
+
+    for activity in activities:
+        activity = activity.to_dict()
+        activity_dict[activity["id"]] = activity
+    
+    activity_json = json.dumps(activity_dict, indent=4, sort_keys=True, default=str)
+
+    return activity_json
+
+
+#WIP
+def get_most_recent_local_activity():
+    file_activities = open("activities.json", "r")
+    file_activities_contents = file_activities.read()
+    activities = json.loads(file_activities_contents)
+    file_activities.close()
+
 
 def get_token():
+    # Get locally stored token
+    token = get_local_token()
+
+    # If token has expired, get new token
+    if int(time.time()) > token["expires_at"]:
+        get_new_token()
+        token = get_local_token()
+    
+    return token
+
+
+def get_local_token():
     file_token = open("token.json", "r")
     file_contents = file_token.read()
     token = json.loads(file_contents)
     file_token.close()
-
-    currentime = int(time.time())
-
-    if currentime > token["expires_at"]:
-        token = get_new_token()
-        file_token = open("token.json", "w")
-        file_token.write(token)
-        file_token.close()
-    
     return token
-
 
 def get_new_token():
     STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
@@ -59,6 +60,10 @@ def get_new_token():
 
     token = strava_oauth2(client_id=STRAVA_CLIENT_ID, client_secret=STRAVA_CLIENT_SECRET)
     token = json.dumps(token)
+
+    file_token = open("token.json", "w")
+    file_token.write(token)
+    file_token.close()
 
     return token
 
@@ -116,15 +121,17 @@ def setup_dotenv():
 
 
 def main():
+    # Check and load .env
     setup_dotenv()
     dotenv.load_dotenv()
-    #token = get_token()
-    #client = StravaIO(access_token=token['access_token'])
-    #get_most_recent_local_activity()
 
-    #if token is not None:
-    #    everything_else(client, "2020-05-31")
-    #    return
+    # Get token
+    token = get_token()
+    client = StravaIO(access_token=token['access_token'])
+
+    # Get all activities and write to file
+    activity_json = get_activities(client)
+    write_activities_to_file(activity_json)
 
 
 if __name__ == "__main__":
